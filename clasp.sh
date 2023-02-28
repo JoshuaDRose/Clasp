17142a4c5b7ebcd33dbe0aa484fbec7b8787f843
Joshua Rose <joshuarose099@gmail.com>
2023-03-01 05:25:53 +1100

    feat: clasp entry file

---
+++ clasp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+show_help_menu ()
+{
+    echo "Help menu"
+    echo "   ...   details.... "
+    echo "   ...   details...."
+    echo "   ...   details...."
+    echo "   ...   details...."
+}
